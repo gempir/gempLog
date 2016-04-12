989a004b8ee32e1ca4abf0c4fb8139d0ff44b1d1
gempir <daniel.pasch.s@gmail.com>
2016-04-12 23:04:02 +0200

    compile regexes only once not everytime a message is parsed

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
-	"regexp"
-	"database/sql"
-	"strings"
 	"bufio"
-	"time"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/op/go-logging"
 	"net"
 	"net/textproto"
 	"os"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/op/go-logging"
+	"regexp"
+	"strings"
+	"time"
 )
 
 var (
-	db, err = sql.Open("mysql", mysql)
-    mainconn net.Conn
+	db, err    = sql.Open("mysql", mysql)
+	mainconn   net.Conn
 	connactive = false
-	log    = logging.MustGetLogger("example")
-	format = logging.MustStringFormatter(
+	log        = logging.MustGetLogger("example")
+	format     = logging.MustStringFormatter(
 		`%{color}[%{time:2006-01-02 15:04:05}] [%{level:.4s}] %{color:reset}%{message}`,
 	)
+	userrp    = regexp.MustCompile(`:\w+!\w+@\w+\.tmi\.twitch\.tv`)
+	channelrp = regexp.MustCompile(`#\w+\s:`)
+	actionrp1 = regexp.MustCompile(`^\x{0001}ACTION\s`)
+	actionrp2 = regexp.MustCompile(`([\x{0001}]+)`)
 )
 
 func main() {
@@ -36,7 +40,7 @@ func main() {
 }
 
 func createConnection() {
-    conn, err := net.Dial("tcp", twitchAddress)
+	conn, err := net.Dial("tcp", twitchAddress)
 	if err != nil {
 		log.Error(err)
 		return
@@ -45,7 +49,7 @@ func createConnection() {
 	fmt.Fprintf(conn, "PASS %s\r\n", twitchOauth)
 	fmt.Fprintf(conn, "USER %s\r\n", twitchUsername)
 	fmt.Fprintf(conn, "NICK %s\r\n", twitchUsername)
-	 // enable roomstate and such
+	// enable roomstate and such
 	log.Info("JOIN #gempbot")
 	fmt.Fprintf(conn, "JOIN %s\r\n", "#gempbot")
 	go startDefaultJoin(conn)
@@ -58,7 +62,7 @@ func createConnection() {
 			log.Error(err)
 			break // break loop on errors
 		}
-		messages := strings.Split(line,"\r\n")
+		messages := strings.Split(line, "\r\n")
 		if len(messages) == 0 {
 			continue
 		}
@@ -73,20 +77,17 @@ func parseMessage(msg string) {
 	if !strings.Contains(msg, ".tmi.twitch.tv PRIVMSG ") {
 		return
 	}
-	userrp := regexp.MustCompile(`:\w+!\w+@\w+\.tmi\.twitch\.tv`)
+
 	fulluser := userrp.FindString(msg)
 	userirc := strings.Split(fulluser, "!")
 	username := userirc[0][1:len(userirc[0])]
 	split2 := strings.Split(msg, ".tmi.twitch.tv PRIVMSG ")
-	rp := regexp.MustCompile(`#\w+\s:`)
-	split3 := rp.FindString(split2[1])
-	channel := split3[0:len(split3)-2]
+	split3 := channelrp.FindString(split2[1])
+	channel := split3[0 : len(split3)-2]
 	split4 := strings.Split(split2[1], split3)
 	message := split4[1]
-	rp2 := regexp.MustCompile(`^\x{0001}ACTION\s`)
-	rp3 := regexp.MustCompile(`([\x{0001}]+)`)
-	message = rp2.ReplaceAllLiteralString(message, "")
-	message = rp3.ReplaceAllLiteralString(message, "")
+	message = actionrp1.ReplaceAllLiteralString(message, "")
+	message = actionrp2.ReplaceAllLiteralString(message, "")
 	timestamp := time.Now().Format("2006-01-2 15:04:05")
 
 	saveMessage(channel, username, message, timestamp)
@@ -94,12 +95,12 @@ func parseMessage(msg string) {
 
 func saveMessage(channel, username, message, timestamp string) {
 	_, err := db.Exec("INSERT INTO gempLog (channel, username, message, timestamp) VALUES (?, ?, ?, ?)", channel, username, message, timestamp)
-    checkErr(err)
+	checkErr(err)
 }
 
 func join(channel string, conn net.Conn) {
 	log.Info("JOIN " + channel)
-    fmt.Fprintf(conn, "JOIN %s\r\n", channel)
+	fmt.Fprintf(conn, "JOIN %s\r\n", channel)
 }
 
 func startDefaultJoin(conn net.Conn) {
