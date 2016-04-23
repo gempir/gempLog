97fad05edee820da317e152336cd2ac833443f78
gempir <daniel.pasch.s@gmail.com>
2016-04-23 12:09:54 +0200

    fixed flake8 complaints

--- archive.py
+++ archive.py
@@ -11,13 +11,12 @@ month = mydate.strftime("%B")
 rootdir = '/var/gemplog/'
 
 for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-                if re.search(month, subdir, re.IGNORECASE):
-                        continue  # current month should be ignored
-                log = os.path.join(subdir, file)
-                if re.search(".gz", file):
-                        continue  # already gzipped this
-
-                with open(log, 'rb') as f_in, gzip.open(log + '.gz', 'wb') as f_out:
-                        shutil.copyfileobj(f_in, f_out)
-                os.remove(log)
+    for file in files:
+        if re.search(month, subdir, re.IGNORECASE):
+            continue  #  current month should be ignored
+        log = os.path.join(subdir, file)
+        if re.search(".gz", file):
+            continue  #  already gzipped this
+        with open(log, 'rb') as f_in, gzip.open(log + '.gz', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+        os.remove(log)
